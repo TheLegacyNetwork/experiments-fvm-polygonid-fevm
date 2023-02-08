5d1e76cce2c63331ef2b6f97d9f62ef5c7597e6f
Sea Ansley <41924928+motherdomains@users.noreply.github.com>
2023-02-08 20:45:39 +0530

    Update hardhat.config.ts
    Some comment tag clean up
    
    Signed-off-by: Sea Ansley <41924928+motherdomains@users.noreply.github.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+// import dependencies
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
@@ -13,8 +14,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 const DEFAULT_MNEMONIC =
   "test test test test test test test test test test test junk";
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+// List accounts
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -22,9 +22,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
